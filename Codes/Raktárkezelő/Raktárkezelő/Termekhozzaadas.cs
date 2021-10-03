dce016bc0974540dc778341b07430995b7e9c87a
Fbalint <fbalintofficial@gmail.com>
2021-10-03 23:29:36 +0200

    Raktárkezelő program termékek hozzáadása kód részlet befejezve

--- Codes/Raktárkezelő/Raktárkezelő/Termekhozzaadas.cs
+++ Codes/Raktárkezelő/Raktárkezelő/Termekhozzaadas.cs
@@ -1,12 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
 
-namespace Raktárkezelő
+
+namespace WindowsFormsApp1
 {
-    class Termekhozzaadas
+
+    public partial class Termekhozzaadas : Form
     {
+        Form1 f1 = (Form1)Application.OpenForms["form1"];
+        public Termekhozzaadas()
+        {
+            InitializeComponent();
+            foreach (var item in Enum.GetValues(typeof(Termekek.Kategoria)))
+            {
+                termekKat.Items.Add(item);
+            }
+        }
+
+
+
+        private void termHozzadButt_Click(object sender, EventArgs e)
+        {
+
+            bool vanE = true;
+            foreach (var item in f1.termek)
+            {
+                if (item.TermekAzonosito == termAz.Text)
+                {
+                    MessageBox.Show("Nem lehet két ugyanolyan termékazonosító!!");
+                    vanE = false;
+                }
+            }
+            if (vanE)
+            {
+
+                Termekek ter = new Termekek(termAz.Text, termNev.Text, (Termekek.Kategoria)termekKat.SelectedItem, Convert.ToInt32(termekDb.Text));
+                f1.listahozAdas(ter);
+                f1.listaUjraToltes();
+
+                f1.fajlKiir("termekek_Kiir.txt");
+
+                this.Close();
+
+            }
+        }
     }
 }
