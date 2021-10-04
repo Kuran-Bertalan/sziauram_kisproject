30495a88177b53f5001ad1faa1721b3653f60604
Ardó Balázs <ardobalazs3@gmail.com>
2021-10-04 19:05:10 +0200

    Raktárkezelő program Termekhozzaadasa.cs teljes kódrészlet feltöltése

--- Codes/Raktárkezelő/Termekhozzaadas.cs
+++ Codes/Raktárkezelő/Termekhozzaadas.cs
@@ -12,9 +12,42 @@ namespace Raktárkezelő
 {
     public partial class Termekhozzaadas : Form
     {
+        Form1 f1 = (Form1)Application.OpenForms["form1"];
         public Termekhozzaadas()
         {
             InitializeComponent();
+            foreach (var item in Enum.GetValues(typeof(Termekek.Kategoria)))
+            {
+                termekKat.Items.Add(item);
+            }
+        }
+
+
+
+        private void termHozzadButt_Click(object sender, EventArgs e)
+        {
+
+            bool vanE = true;
+            foreach (var item in f1.termek)
+            {
+                if (item.TermekAzonosito == termAz.Text)
+                {
+                    MessageBox.Show("Nem lehet két ugyanolyan termékazonosító!!");
+                    vanE = false;
+                }
+            }
+            if (vanE)
+            {
+
+                Termekek ter = new Termekek(termAz.Text, termNev.Text, (Termekek.Kategoria)termekKat.SelectedItem, Convert.ToInt32(termekDb.Text));
+                f1.listahozAdas(ter);
+                f1.listaUjraToltes();
+
+                f1.fajlKiir("termekek_Kiir.txt");
+
+                this.Close();
+
+            }
         }
     }
 }
