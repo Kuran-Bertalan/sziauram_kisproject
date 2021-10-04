a1b93fcedd0d08c2c562f9adacc11f17421572b1
Costa-31 <bodozolii999@gmail.com>
2021-10-04 18:13:48 +0200

    A fájl feltöltések és a datagradeview1 feltöltése

--- Codes/Raktárkezelő/Form1.cs
+++ Codes/Raktárkezelő/Form1.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
 namespace Raktárkezelő
 {
     public partial class Form1 : Form
     {
+        public List<Termekek> termek = new List<Termekek>();
+
         public Form1()
         {
             InitializeComponent();
+
+            fajlBeolvas("termekek_Kiir.txt");
+            dataGridView1.DataSource = termek;
+
+        }
+
+        public void fajlBeolvas(string beolv)
+        {
+            StreamReader sr = new StreamReader(beolv);
+            string[] atmenet;
+            while (!sr.EndOfStream)
+            {
+                atmenet = sr.ReadLine().Split(';');
+                termek.Add(new Termekek(atmenet[0], atmenet[1], (Termekek.Kategoria)Enum.Parse(typeof(Termekek.Kategoria), atmenet[2]), Convert.ToInt32(atmenet[3])));
+            }
+            sr.Close();
         }
     }
 }
